1ff53ebecaaba636ad27f81d93c1ad9709702447
deculcaca <winsantul8636@gmail.com>
2024-11-20 17:15:28 +0700

    git commit -m "perubahan ke 4 "

--- PROYEK ALGO 1/projek akhir.py
+++ PROYEK ALGO 1/projek akhir.py
@@ -28,7 +28,7 @@ def register():
         if otw_login == '2':
             print("=-=-=-=-=-=- Pendaftaran selesai! -=-=-=-=-=-=")
             input("klik enter untuk login")
-        elif otw_login== '1':
+        elif otw_login == '1':
             login()
         else:
             print("input tidak tersedia")
@@ -45,13 +45,13 @@ def login():
     password = input("Masukkan password: ").strip()
 
     if not os.path.isfile('RegTRIJAYA.csv'): 
-        print("Belum ada akun yang terdaftar. Silakan daftar terlebih dahulu.")
+        print("Akun tidak terdaftar. Silakan daftar terlebih dahulu!.")
         return
 
     with open('RegTRIJAYA.csv', mode='r') as file:
         reader = csv.DictReader(file)
-        for row in reader:
-            if row['username'] == username and row['password'] == password:
+        for i in reader:
+            if i['username'] == username and i['password'] == password:
                 print(f"Selamat datang, {username}!")
                 return
 
@@ -59,14 +59,126 @@ def login():
 register()
 login()
 
-# def jenis_produk():
-#     pass
+def jenis_produk(username):
+    print(f"Selamat datang {username}, silahkan pilih jenis produk : ")
+    print('''
+    1. Alat persiapan lahan
+    2. Alat penanaman
+    3. Alat pemeliharaan tanaman
+    4. Alat panen
+    5. Alat pascapanen
+    6. Bahan pertanian
+    7. Pencarian
+    ''')
+    jenis = input("Pilihlah jenis produk (1/2/3/4/5/6): ")
+    if jenis == '1':
+        print('''
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+          $$$$$$$$$^^^^^  Alat Persiapan Lahan  ^^^^^$$$$$$$$
+        __________________ SELAMAT BERBELANJA ___________________
+        1. nama_produk ({stok})
+        2. nama_produk ({stok})
+        3. nama_produk ({stok})
+        4. nama_produk ({stok})
+        5. nama_produk ({stok})
+        6. nama_produk ({stok})
+        7. nama_produk ({stok})
+        8. nama_produk ({stok})
+        9. kembali
 
-# def daftar_produk():
-#     pass
+        A. Masukkan keranjang
+        B. Beli dan bayar
+        ''')
+        
+    elif jenis == '2':
+        print('''
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+          $$$$$$$$$^^^^^  Alat Penanaman  ^^^^^$$$$$$$$
+        __________________ SELAMAT BERBELANJA ___________________
+        1. nama_produk ({stok})
+        2. nama_produk ({stok})
+        3. nama_produk ({stok})
+        4. nama_produk ({stok})
+        5. nama_produk ({stok})
+        6. nama_produk ({stok})
+        7. nama_produk ({stok})
+        8. nama_produk ({stok})
 
-# def keranjang():
-#     pass
+        A. Masukkan keranjang
+        B. Beli dan bayar
+        ''')
+    elif jenis == '3':
+        print('''
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+          $$$$$$$$$^^^^^  Alat Pemeliharaan Tanaman  ^^^^^$$$$$$$$
+        __________________ SELAMAT BERBELANJA ___________________
+        1. nama_produk ({stok})
+        2. nama_produk ({stok})
+        3. nama_produk ({stok})
+        4. nama_produk ({stok})
+        5. nama_produk ({stok})
+        6. nama_produk ({stok})
+        7. nama_produk ({stok})
+        8. nama_produk ({stok})
+
+        A. Masukkan keranjang
+        B. Beli dan bayar
+        ''')
+    elif jenis == '4':
+        print('''
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+          $$$$$$$$$^^^^^  Alat Panen  ^^^^^$$$$$$$$
+        __________________ SELAMAT BERBELANJA ___________________
+        1. nama_produk ({stok})
+        2. nama_produk ({stok})
+        3. nama_produk ({stok})
+        4. nama_produk ({stok})
+        5. nama_produk ({stok})
+        6. nama_produk ({stok})
+        7. nama_produk ({stok})
+        8. nama_produk ({stok})
+
+        A. Masukkan keranjang
+        B. Beli dan bayar
+        ''')
+    elif jenis == '5':
+        print('''
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+          $$$$$$$$$^^^^^  Alat Pascapanen  ^^^^^$$$$$$$$
+        __________________ SELAMAT BERBELANJA ___________________
+        1. nama_produk ({stok})
+        2. nama_produk ({stok})
+        3. nama_produk ({stok})
+        4. nama_produk ({stok})
+        5. nama_produk ({stok})
+        6. nama_produk ({stok})
+        7. nama_produk ({stok})
+        8. nama_produk ({stok})
+
+        A. Masukkan keranjang
+        B. Beli dan bayar
+        ''')
+    elif jenis == '6':
+        print('''
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+          $$$$$$$$$^^^^^  Bahan Pertanian  ^^^^^$$$$$$$$
+        __________________ SELAMAT BERBELANJA ___________________
+        1. nama_produk ({stok})
+        2. nama_produk ({stok})
+        3. nama_produk ({stok})
+        4. nama_produk ({stok})
+        5. nama_produk ({stok})
+        6. nama_produk ({stok})
+        7. nama_produk ({stok})
+        8. nama_produk ({stok})
+
+        A. Masukkan keranjang
+        B. Beli dan bayar
+        ''')
+    elif jenis == '7':
+        pass
+def keranjang():
+      pass
 
 # def deskripsi_produk():
 #     pass
@@ -81,4 +193,4 @@ login()
 #     pass
 
 # def pembayaran():
-#     pass
+    
